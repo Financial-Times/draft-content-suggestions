57bdb1aa610d63a757568aae5890e68a7869f4ff
Erdem Aslan <erdem.aslan@ft.com>
2018-03-12 14:18:14 +0000

    Refactored impl for readability.

--- handler.go
+++ handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"net"
 
 	"github.com/Financial-Times/draft-content-suggestions/commons"
 	"github.com/Financial-Times/draft-content-suggestions/draft"
@@ -30,64 +31,49 @@ func (rh *requestHandler) draftContentSuggestionsRequest(writer http.ResponseWri
 		return
 	}
 
-	responseCh := make(chan []byte)
-	errorCh := make(chan error)
-
 	now := time.Now()
 	ctx, cancelCtx := context.WithTimeout(commons.NewContextFromRequest(request), now.Add(rh.srt).Sub(now))
 	defer cancelCtx()
 
-	go func(ctx context.Context, responseCh chan []byte, errorCh chan error) {
-		content, err := rh.dca.FetchDraftContent(ctx, uuid)
-		if err != nil {
-			errorCh <- err
+	content, err := rh.dca.FetchDraftContent(ctx, uuid)
+
+	if err != nil {
+		netError, assertion := err.(net.Error)
+		if assertion && netError.Timeout() {
+			log.WithField("uuid", uuid).Error("Timed out processing draft content suggestions request during fetching draft content")
+			commons.WriteJSONMessage(writer, http.StatusRequestTimeout, "Draft content api access has timed out.")
 			return
 		}
-		responseCh <- content
-	}(ctx, responseCh, errorCh)
 
-	var content []byte
-
-	select {
-	case <-ctx.Done():
-		log.WithField("uuid", uuid).Error("Timed out processing draft content suggestions request during fetching draft content")
-		commons.WriteJSONMessage(writer, http.StatusRequestTimeout, "Draft content api access has timed out.")
-		return
-	case err := <-errorCh:
 		log.WithError(err).WithField("uuid", uuid).Error("Draft content api access has failed.")
 		commons.WriteJSONMessage(writer, http.StatusServiceUnavailable, "Draft content api access has failed.")
 		return
-	case content = <-responseCh:
-		if content == nil {
-			commons.WriteJSONMessage(writer, http.StatusNotFound, fmt.Sprintf("No draft content for uuid: %v", uuid))
-			return
-		}
 	}
 
-	go func(ctx context.Context, responseChan chan []byte, errorCh chan error) {
-		suggestion, err := rh.sua.FetchSuggestions(ctx, content)
-		if err != nil {
-			errorCh <- err
+	if content == nil {
+		commons.WriteJSONMessage(writer, http.StatusNotFound, fmt.Sprintf("No draft content for uuid: %v", uuid))
+		return
+	}
+
+	suggestion, err := rh.sua.FetchSuggestions(ctx, content)
+
+	if err != nil {
+		netError, assertion := err.(net.Error)
+		if assertion && netError.Timeout() {
+			log.WithField("uuid", uuid).Error("Timed out processing draft content suggestions request during suggestions umbrella api access")
+			commons.WriteJSONMessage(writer, http.StatusRequestTimeout, "Suggestions Umbrella api access has timed out.")
 			return
 		}
-		responseCh <- suggestion
-	}(ctx, responseCh, errorCh)
 
-	select {
-	case <-ctx.Done():
-		log.WithField("uuid", uuid).Error("Timed out processing draft content suggestions request during suggestions umbrella api access")
-		commons.WriteJSONMessage(writer, http.StatusRequestTimeout, "Suggestions Umbrella api access has timed out.")
-		return
-	case err := <-errorCh:
 		log.WithError(err).WithField("uuid", uuid).Error("Suggestions umbrella api access has failed")
 		commons.WriteJSONMessage(writer, http.StatusServiceUnavailable, "Suggestions umbrella api access has failed")
 		return
-	case suggestion := <-responseCh:
-		writer.Header().Set("Content-Type", "application/json")
-		_, err = writer.Write(suggestion)
+	}
 
-		if err != nil {
-			log.WithError(err).WithField("uuid", uuid).Error("Failed responding to draft content suggestions request")
-		}
+	writer.Header().Set("Content-Type", "application/json")
+	_, err = writer.Write(suggestion)
+
+	if err != nil {
+		log.WithError(err).WithField("uuid", uuid).Error("Failed responding to draft content suggestions request")
 	}
 }
